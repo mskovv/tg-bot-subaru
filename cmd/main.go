69532b1816bdb075156676f7a906ddfb087f4694
mskovv <maslay99@gmail.com>
2024-10-30 22:08:49 +0500

    init #2

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,7 @@
 package main
+
+import "github.com/mskovv/tg-bot-subaru96/database"
+
+func main() {
+	database.ConnectDb()
+}
--- models/models.go
+++ models/models.go
@@ -1,1 +1,10 @@
 package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name" gorm:"text;not null;default:null`
+	LastName string `json:"LastName" gorm:"text;not null;default:null`
+	Nickname string `json:"Nickname" gorm:"text;not null;default:null`
+}
